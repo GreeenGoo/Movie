92d6a0ea034f36ee018202e8d9b4de3391c43b18
PC <uladzislau.krukosuki@gmail.com>
2022-11-06 19:30:17 +0200

    Created a pattern for movie to describe it.

--- app/src/main/java/com/education/movie/presentation/fragments/movie/MovieFragment.kt
+++ app/src/main/java/com/education/movie/presentation/fragments/movie/MovieFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.education.movie.R
 import com.education.movie.presentation.viewmodel.mainscreen.MainScreenViewModel
-import kotlinx.android.synthetic.main.fragment_movie.text_view
 
 class MovieFragment : Fragment() {
 
@@ -21,6 +20,6 @@ class MovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val bundleNote = arguments?.getInt(MainScreenViewModel.BUNDLE_KEY)
-        text_view.text = bundleNote.toString()
+
     }
 }
