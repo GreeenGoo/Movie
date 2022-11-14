77b7894825583920336439b1839a396b10fa2f46
PC <uladzislau.krukosuki@gmail.com>
2022-11-14 15:22:38 +0200

    Fixed bugs and renamed some classes

--- app/src/main/java/com/education/movie/presentation/MainActivity.kt
+++ app/src/main/java/com/education/movie/presentation/MainActivity.kt
@@ -2,16 +2,29 @@ package com.education.movie.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.education.movie.R
-import com.education.movie.presentation.fragments.MoviesFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, MoviesFragment())
-            .commit()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || findNavController(R.id.fragment_container).navigateUp()
     }
 }
